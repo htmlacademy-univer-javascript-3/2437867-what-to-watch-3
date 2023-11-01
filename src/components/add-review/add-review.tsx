daad15f0b20fcf8fc7dc916cdc2205a52fc7fea0
Matvey Ilichev <matvey.ilichev@gmail.com>
2023-11-01 20:00:41 +0500

    player

--- src/components/add-review/add-review.tsx
+++ src/components/add-review/add-review.tsx
@@ -2,6 +2,8 @@ import {useState} from 'react';
 
 function AddReview() {
   const [text, setText] = useState<string>('');
+  const [rating, setRating] = useState<number>(10);
+
   return (
     <div className="add-review">
       <form action="#" className="add-review__form">
@@ -10,7 +12,7 @@ function AddReview() {
             {Array.from({length: 10}).map((_, index) => (
               <>
                 <input className="rating__input" id={`star-${10 - index}`} type="radio" name="rating"
-                  value={10 - index}
+                       value={10 - rating} onChange={(evt) => setRating(+evt.target.value)}
                 />
                 <label className="rating__label" htmlFor={`star-${10 - index}`}>Rating {10 - index}</label>
               </>
@@ -20,7 +22,7 @@ function AddReview() {
 
         <div className="add-review__text">
           <textarea className="add-review__textarea" name="review-text" id="review-text"
-            placeholder="Review text" value={text} onChange={(evt) => setText(evt.target.value)}
+                    placeholder="Review text" value={text} onChange={(evt) => setText(evt.target.value)}
           >
           </textarea>
           <div className="add-review__submit">
--- src/components/film-card/film-card.tsx
+++ src/components/film-card/film-card.tsx
@@ -1,27 +1,41 @@
-import {useNavigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import {AppRoute} from "../../consts.ts";
+import VideoPlayer from "../video-player/video-player.tsx";
+import {useState} from "react";
 
 export type FilmCardProps = {
   id: string;
   posterSrc: string;
   posterAlt: string;
   title: string;
-  onMouseEnter: (x: string) => void;
-  onMouseLeave: () => void;
+  videoLink: string,
 }
 
 function FilmCard(props: FilmCardProps) {
-  const navigate = useNavigate();
+  const [timer, setTimer] = useState<NodeJS.Timeout | undefined>(undefined);
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
+  const onMouseEnter = () => {
+    setTimer(setTimeout(() => setIsHovered(true), 1000));
+  }
+
+  const onMouseLeave = () => {
+    clearTimeout(timer);
+    setIsHovered(false)
+  }
+
   return (
-    <article className="small-film-card catalog__films-card" onMouseEnter={() => props.onMouseEnter(props.id)}
-      onMouseLeave={() => props.onMouseLeave()} onClick={() => navigate(`/films/${props.id}`)}
-    >
+    <article className="small-film-card catalog__films-card" onMouseEnter={onMouseEnter}
+             onMouseLeave={onMouseLeave}>
       <div className="small-film-card__image">
-        <img src={props.posterSrc}
-          alt={props.posterAlt}
-        />
+        {
+          isHovered ?
+            <VideoPlayer videoLink={props.videoLink} posterSrc={props.posterSrc}/> :
+            <img src={props.posterSrc} alt={props.posterAlt}/>
+        }
       </div>
       <h3 className="small-film-card__title">
-        <a className="small-film-card__link">{props.title}</a>
+        <Link to={AppRoute.Movie + `/${props.id}`} className="small-film-card__link">{props.title}</Link>
       </h3>
     </article>
   );
